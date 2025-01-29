d705c9b8b321a891a8938da397f01eedad567a6e
Esraaelsisy <esraa.elsisy@gmail.com>
2025-01-29 14:49:44 +0100

    Renaming requestBuilder for better name convention in planets.api.ts file

--- e2e/api-tests/api/planets.api.ts
+++ e2e/api-tests/api/planets.api.ts
@@ -1,24 +1,24 @@
 import { RequestBuilder } from "../utlis/requestBuilder";
 
 export class PlanetsApi {
-  private requestHelper: RequestBuilder;
+  private requestBuilder: RequestBuilder;
   private endpoints: Record<string, string>;
 
   constructor(
     requestHelper: RequestBuilder,
     endpoints: Record<string, string>
   ) {
-    this.requestHelper = requestHelper;
+    this.requestBuilder = requestHelper;
     this.endpoints = endpoints;
   }
 
   async getPlanetById(id: number) {
-    return this.requestHelper.get(this.endpoints.planetsById, {
+    return this.requestBuilder.get(this.endpoints.planetsById, {
       planet_id: id.toString(),
     });
   }
 
   async getAllPlanets() {
-    return this.requestHelper.get(this.endpoints.planets);
+    return this.requestBuilder.get(this.endpoints.planets);
   }
 }
