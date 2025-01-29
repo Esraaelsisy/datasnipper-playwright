70c9bdecd8567497af569521185628ed788646fc
Esraaelsisy <esraa.elsisy@gmail.com>
2025-01-29 14:50:50 +0100

    Isolating the locators from the methods in pricing.page.ts file

--- e2e/ui-tests/pages/pricing.page.ts
+++ e2e/ui-tests/pages/pricing.page.ts
@@ -1,38 +1,47 @@
-import { Page } from "@playwright/test";
+import { Page, Locator } from "@playwright/test";
 
 export class PricingPage {
-  constructor(private page: Page) {}
+  private packageNamesLocator: Locator;
+  private bookDemoLinksLocator: Locator;
+  private enterpriseCustomOCRCheckbox: Locator;
 
-  async navigateToPricingPage() {
+  constructor(private page: Page) {
+    this.packageNamesLocator = this.page.locator(
+      ".price-card-v-two .heading-5-card-pricing.title"
+    );
+    this.bookDemoLinksLocator = this.page.locator(
+      ".button-secundary-blue.margin-top.low.w-button"
+    );
+    this.enterpriseCustomOCRCheckbox = this.page
+      .getByText("Custom OCR Configuration")
+      .locator("flex-pricing-checks-overview")
+      .locator("img.image-45");
+  }
+
+  async navigateToPricingPage(): Promise<void> {
     await this.page.goto("/pricing");
   }
 
   async getPackageNames(): Promise<string[]> {
-    const packageNames = await this.page
-      .locator(".price-card-v-two .heading-5-card-pricing.title")
-      .evaluateAll((elements) =>
-        elements.map((el) => el.textContent?.trim() || "")
-      );
+    const packageNames = await this.packageNamesLocator.evaluateAll(
+      (elements) => elements.map((el) => el.textContent?.trim() || "")
+    );
     return [...new Set(packageNames)];
   }
 
   async getBookDemoLinks(): Promise<string[]> {
-    return await this.page
-      .locator(".button-secundary-blue.margin-top.low.w-button")
-      .evaluateAll((elements) =>
-        elements.map((el) => el.getAttribute("href") || "")
-      );
+    const bookDemoLinks = await this.bookDemoLinksLocator.evaluateAll(
+      (elements) => elements.map((el) => el.getAttribute("href") || "")
+    );
+    return [...new Set(bookDemoLinks)];
   }
 
   async isCustomOCRAvailableForEnterprise(): Promise<boolean> {
-    const enterpriseCheckbox = this.page
-      .getByText("Custom OCR Configuration")
-      .locator("flex-pricing-checks-overview")
-      .locator("img.image-45");
-    await enterpriseCheckbox.waitFor({ state: "visible" });
-    const isThirdChild = await enterpriseCheckbox.evaluate((img) => {
-      return img === img.parentElement?.children[2];
-    });
+    const isThirdChild = await this.enterpriseCustomOCRCheckbox.evaluate(
+      (img) => {
+        return img === img.parentElement?.children[2];
+      }
+    );
     return isThirdChild;
   }
 }
