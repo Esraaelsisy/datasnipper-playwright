3270aeeebb1bc71dea893e8c1e489593958a265c
Esraaelsisy <esraa.elsisy@gmail.com>
2025-01-29 14:49:33 +0100

    Renaming requestBuilder for better name convention in people.api.ts file

--- e2e/api-tests/api/people.api.ts
+++ e2e/api-tests/api/people.api.ts
@@ -1,24 +1,24 @@
 import { RequestBuilder } from "../utlis/requestBuilder";
 
 export class PeopleApi {
-  private requestHelper: RequestBuilder;
+  private requestBuilder: RequestBuilder;
   private endpoints: Record<string, string>;
 
   constructor(
     requestHelper: RequestBuilder,
     endpoints: Record<string, string>
   ) {
-    this.requestHelper = requestHelper;
+    this.requestBuilder = requestHelper;
     this.endpoints = endpoints;
   }
 
   async getPersonById(id: number) {
-    return this.requestHelper.get(this.endpoints.peopleById, {
+    return this.requestBuilder.get(this.endpoints.peopleById, {
       people_id: id.toString(),
     });
   }
 
   async getAllPeople() {
-    return this.requestHelper.get("/people/");
+    return this.requestBuilder.get(this.endpoints.people);
   }
 }
