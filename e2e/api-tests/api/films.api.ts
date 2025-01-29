32d88fe27ca08746a141a0a4d5f283544c5c49ab
Esraaelsisy <esraa.elsisy@gmail.com>
2025-01-29 14:49:26 +0100

    Renaming requestBuilder for better name convention in films.api.ts file

--- e2e/api-tests/api/films.api.ts
+++ e2e/api-tests/api/films.api.ts
@@ -1,18 +1,18 @@
 import { RequestBuilder } from "../utlis/requestBuilder";
 
 export class FilmsApi {
-  private requestHelper: RequestBuilder;
+  private requestBuilder: RequestBuilder;
   private endpoints: Record<string, string>;
 
   constructor(
     requestHelper: RequestBuilder,
     endpoints: Record<string, string>
   ) {
-    this.requestHelper = requestHelper;
+    this.requestBuilder = requestHelper;
     this.endpoints = endpoints;
   }
 
   async getFilmByTitle(title: string) {
-    return this.requestHelper.get(this.endpoints.films, undefined, { title });
+    return this.requestBuilder.get(this.endpoints.films, undefined, { title });
   }
 }
